3df68771edc3af4f34435c9a0971c7e8b56b755a
Gilberto Cruz <58277487+gibadeives@users.noreply.github.com>
2022-03-12 18:19:00 -0300

    Cód ao final da primeira aula imersão react Alura

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,40 @@
 import styled from 'styled-components'
+import db from '../db.json'
+import Widget from '../src/components/Widget'
+import FooterWrapper from '../src/components/Footer'
+import GitHubCorner from '../src/components/GitHubCorner'
+import QuizBackground from '../src/components/QuizBackground'
+import QuizContainer from '../src/components/QuizContainer'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={db.bg} >
+      <QuizContainer>
+        <Widget>
+          <h1>Titulo</h1>
+          <Widget.Header>
+            <Widget.Content>
+              <p>Bla, bla, bla bla bla</p>
+            </Widget.Content>
+          </Widget.Header>
+        </Widget>
+        
+        <Widget>
+          <h1>Titulo 2</h1>
+          <Widget.Header>
+            <Widget.Content>
+              <p>Bla, bla, bla bla bla</p>
+            </Widget.Content>
+          </Widget.Header>
+        </Widget>
+
+        
+        <FooterWrapper />
+      </QuizContainer>
+      <GitHubCorner projectUrl={"https://github.com/gibadeives"} />
+    </QuizBackground>
+
+  )
+
 }
