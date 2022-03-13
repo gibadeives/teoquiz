de3c7d252810851e78dd1f7f5aff050def5db33a
Gilberto Cruz <58277487+gibadeives@users.noreply.github.com>
2022-03-13 17:40:03 -0300

    Alterando .7

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
 import db from '../db.json'
+import Head from 'next/head'
+import React from 'react'
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -14,8 +16,13 @@ const theme = db.theme
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="head" />
+      </Head>
       <ThemeProvider theme={theme}>
-      <GlobalStyle />
+        <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
     </>
--- pages/index.js
+++ pages/index.js
@@ -5,17 +5,42 @@ import FooterWrapper from '../src/components/Footer'
 import GitHubCorner from '../src/components/GitHubCorner'
 import QuizBackground from '../src/components/QuizBackground'
 import QuizContainer from '../src/components/QuizContainer'
+import Head from 'next/head'
+import React from 'react'
+import { useRouter } from 'next/router'
 
 
 export default function Home() {
+  const router = useRouter()
+  const [name, setName] = React.useState('')
+  console.log('Retorno do useState', name, setName)
+
   return (
     <QuizBackground backgroundImage={db.bg} >
+      <Head>
+        <title>TEOQUIZ - W A R</title>
+      </Head>
       <QuizContainer>
         <Widget >
           <h1>#TEOQUIZ</h1>
           <Widget.Header>
             <Widget.Content>
-              <p>Clique aqui para jogar</p>
+
+              <form onSubmit={function (infosDoEvento) {
+                infosDoEvento.preventDefault()
+                router.push(`/quiz?name=${name}`)
+                console.log('Fazendo uma submissão por meio do React')
+                //router manda para a próxima página
+              }} >
+
+                <input onChange={function (infosDoEvento) {
+                  console.log(infosDoEvento.target.value)
+                  //state
+                  setName(infosDoEvento.target.value)
+                }} placeholder='Insira seu nome' />
+                <button type="submit" disabled={name.length === 0}>Jogar - {name}</button>
+              </form>
+
             </Widget.Content>
           </Widget.Header>
         </Widget>
